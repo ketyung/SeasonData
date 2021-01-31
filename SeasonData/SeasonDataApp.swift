1edd4a552acb6299dcb8cf0261230c9f8bb0e5fb
Christopher Chee <ketyung@techchee.com>
2021-01-31 09:58:14 +0800

    Initial Commit

---
+++ SeasonData/SeasonDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonDataApp.swift
+//  SeasonData
+//
+//  Created by Chee Ket Yung on 31/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SeasonDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
